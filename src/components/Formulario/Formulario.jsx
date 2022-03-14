55bfb8ff74ec9f40c6c18fafaaf249c618072a6a
KevinSangregorio <kesangregorio@gmail.com>
2022-03-14 14:45:53 -0300

    [MOD] consumo de contexto, utilizo hook ahora

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -15,55 +15,52 @@ import { FormDataContext } from "../../context/ContextoFormulario";
  * @returns {component}
  */
 const Formulario = () => {
+  const { state } = useContext(FormDataContext);
 
   return (
-    <FormDataContext.Consumer>
-      {({ state }) => (
-        <>
-          <header className="form-header">
+    <>
+      <header className="form-header">
+        <div>
+          <img src={pokebola} alt="pokebola" />
+          <h2>Centro Pokemon de Ash</h2>
+        </div>
+        <Link className="volver" to="/">
+          Home
+        </Link>
+      </header>
+      <div className="formulario-ingreso">
+        <h3>Solicitud de atención</h3>
+        <p>
+          Por favor, completa el formulario para que podamos atender a tu
+          pokémon
+        </p>
+        <div className="cuerpo-formulario">
+          <div className="inputs">
             <div>
-              <img src={pokebola} alt="pokebola" />
-              <h2>Centro Pokemon de Ash</h2>
+              <p className="nombre-seccion">
+                <img src={entrenador} alt="entrenador" />
+                <span>ENTRENADOR</span>
+              </p>
+              <Input name="nombre" label="Nombre" />
+              <Input name="apellido" label="Apellido" />
+              <Input name="email" label="Email" type="email" />
             </div>
-            <Link className="volver" to="/">
-              Home
-            </Link>
-          </header>
-          <div className="formulario-ingreso">
-            <h3>Solicitud de atención</h3>
-            <p>
-              Por favor, completa el formulario para que podamos atender a tu
-              pokémon
-            </p>
-            <div className="cuerpo-formulario">
-              <div className="inputs">
-                <div>
-                  <p className="nombre-seccion">
-                    <img src={entrenador} alt="entrenador" />
-                    <span>ENTRENADOR</span>
-                  </p>
-                  <Input name="nombre" label="Nombre" />
-                  <Input name="apellido" label="Apellido" />
-                  <Input name="email" label="Email" type="email" />
-                </div>
-                <div>
-                  <p className="nombre-seccion">
-                    <img src={pikachu} alt="pikachu" />
-                    <span>POKEMON</span>
-                  </p>
-                  <Input name="nombrePokemon" label="Nombre" />
-                  <Input name="tipoPokemon" label="Tipo" />
-                  <Input name="elementoPokemon" label="Elemento" />
-                  <Input name="alturaPokemon" label="Altura" type="number" />
-                  <Input name="edadPokemon" label="Edad" type="number" />
-                </div>
-              </div>
-              <Detalle state={state} />
+            <div>
+              <p className="nombre-seccion">
+                <img src={pikachu} alt="pikachu" />
+                <span>POKEMON</span>
+              </p>
+              <Input name="nombrePokemon" label="Nombre" />
+              <Input name="tipoPokemon" label="Tipo" />
+              <Input name="elementoPokemon" label="Elemento" />
+              <Input name="alturaPokemon" label="Altura" type="number" />
+              <Input name="edadPokemon" label="Edad" type="number" />
             </div>
           </div>
-        </>
-      )}
-    </FormDataContext.Consumer>
+          <Detalle state={state} />
+        </div>
+      </div>
+    </>
   );
 };
 

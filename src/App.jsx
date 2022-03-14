a49a736ad923be1e98d1c40aea781b7b51b7f931
KevinSangregorio <kesangregorio@gmail.com>
2022-03-14 14:26:22 -0300

    [ADD] agrego react-query

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Formulario from "./components/Formulario/Formulario";
 import FormDataProvider from "./context/ContextoFormulario";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <FormDataProvider>
-      <div className="App">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/formularioIngreso" element={<Formulario />} />
-        </Routes>
-      </div>
-    </FormDataProvider>
+    <QueryClientProvider cliente={queryClient}>
+      <FormDataProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/formularioIngreso" element={<Formulario />} />
+          </Routes>
+        </div>
+      </FormDataProvider>
+    </QueryClientProvider>
   );
 }
 
